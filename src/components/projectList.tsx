c644ec5bdd77c5e06d34807dc2e1da55725bda01
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2024-03-30 23:27:45 +0900

    fix fit user zod type

--- src/components/projectList.tsx
+++ src/components/projectList.tsx
@@ -1,12 +1,39 @@
 'use client'
 import React, { useMemo } from "react";
 import { Column, PagingTable } from "@yamada-ui/table";
-import { Data } from "@tanstack/react-table"
+import { RowData as Data } from "@yamada-ui/table";
 
 
-const data = fetch("https://jsonplaceholder.typicode.com/posts").catch((error) => {
-  console.error("Error:", error);
-})
+const data = [
+  {
+    title: "hivehack2023",
+    duration_start: "2023/11/23",
+    duration_end: "2023/11/24",
+  },
+  {
+    title: "技育CAMP vol.15",
+    duration_start: "2024/02/25",
+    duration_end: "2024/02/26",
+  },
+  {
+    title: "PRTIMES winter hackathon",
+    duration_start: "2024/02/28",
+    duration_end: "2024/03/01",
+  },
+  {
+    title: "hack U",
+    duration_start: "2024/03/16",
+    duration_end: "2024/03/17",
+  },
+  {
+    title: "giftee intern LINEbot",
+    duration_start: "2024/01/31",
+    duration_end: "2024/02/06",
+  },
+]; 
+// fetch("https://jsonplaceholder.typicode.com/posts").catch((error) => {
+//   console.error("Error:", error);
+// })
 
 const ProjectList = () => {
   const columns = useMemo<Column<Data>[]>(
@@ -21,7 +48,7 @@ const ProjectList = () => {
       },
       {
         header: "CODE FREEZE",
-        accessorKey: "suration_end",
+        accessorKey: "duration_end",
       },
     ],
     [],
